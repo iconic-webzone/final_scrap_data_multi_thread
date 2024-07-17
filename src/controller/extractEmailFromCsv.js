0df23d8069c9a484308a8287b4567101fe7f2d8f
unknown <subhransusekharsahu29@gmail.com>
2024-07-17 23:47:40 +0530

    last-sort

--- src/controller/extractEmailFromCsv.js
+++ src/controller/extractEmailFromCsv.js
@@ -1,9 +1,6 @@
 import getTime from "../../newIndex.js";
 import websiteModel from "../model/websiteModel.js";
-import jsonToCsvConverter from "../utils/JSONTOCSV1.js";
-import json2Csv from "../utils/json2Csv.js";
-import jsonToCsv from "../utils/jsonToCsv.js";
-import readCsv from "../utils/readCsv.js";
+import jsonToCsvConverter from "../utils/jsonToCsv.js";
 import readXlsx from "../utils/readXlsx.js";
 import dataAfterScrapingWebs from "../utils/scrapWebsite.js";
 import processStrings from "../utils/validUrl.js";
@@ -26,14 +23,7 @@ const extractEmailFromUrl = async (req, res) => {
         dataAfterScrapingWebs(httpsAddedUrls)
             .then((data) => {
                 console.log(data)
-                // let jsonData = data.map((obj)=>{
-                //     if(obj !== null) return obj
-                // })
-                // console.log(jsonData);
-                // json2Csv(data);
-                // jsonToCsv(jsonData)
                 jsonToCsvConverter(data)
-                
                 res.json(data)
             })
             .catch((err) => { console.log(err) })
--- src/utils/jsonToCsv.js
+++ src/utils/jsonToCsv.js
@@ -1,49 +1,47 @@
-import { Parser } from 'json2csv';
 import fs from 'fs';
+import path from 'path';
+import getTime from '../../newIndex.js';
 
-function flattenArray(arr) {
-    return arr.join(',');
+const currentDir = path.resolve();
+
+
+const jsonToCsvConverter = (data) => {
+    const escapeCsvValue = (value) => {
+        if (typeof value === 'string' && value.includes(',')) {
+          return `"${value.replace(/"/g, '""')}"`; // Escape quotes
+        }
+        return value;
+      };
+      
+      // Create a writable stream
+      const outputPath = path.join(currentDir,"public/temp", 'output.csv');
+      const writableStream = fs.createWriteStream(outputPath);
+      
+      // Write headers
+      writableStream.write('Emails,URL\n');
+      
+      // Process data and write to stream
+      data.forEach(entry => {
+        if (entry && entry.url) {
+          const emails =entry.emails? entry.emails.join(',') : null; // Join emails with semicolons
+          const row = `${escapeCsvValue(emails)},${escapeCsvValue(entry.url)}\n`;
+          writableStream.write(row);
+        }
+      });
+      
+      // Handle the finish event
+      writableStream.on('finish', () => {
+        console.log('CSV file created successfully.');
+      });
+      
+      // Error handling for the stream
+      writableStream.on('error', (err) => {
+        console.error('Error writing to CSV file', err);
+        
+      });
+      
+      // Close the stream
+      writableStream.end();
 }
 
-const jsonToCsv = (json) => {
-
-    function flattenJSON(json) {
-        return json.map(entry => {
-            const flattenedEntry = {};
-    
-            for (const [key, value] of Object.entries(entry)) {
-                if (Array.isArray(value)) {
-                    flattenedEntry[key] = flattenArray(value);
-                } else {
-                    flattenedEntry[key] = value;
-                }
-            }
-    
-            return flattenedEntry;
-        });
-    }
-    
-    const flattenedJson = flattenJSON(json);
-    
-    const fields = Object.keys(flattenedJson[0]);
-    const opts = { fields };
-    
-    try {
-        const parser = new Parser(opts);
-        const csv = parser.parse(flattenedJson);
-        fs.writeFileSync('./public/temp/new.csv', csv);
-        console.log("file written successfully")
-        console.log(csv);
-    } catch (err) {
-        console.error(err);
-    }
-
-}
-
-export default jsonToCsv ;
-
-
-
-
- 
-
+export default jsonToCsvConverter ;
